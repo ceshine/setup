2f4d21da7fdca6416f01584dee39dff5363ad3e8
CeShine Lee <shuanck@gmail.com>
2013-07-11 15:12:32 +0800

    Add uglify-js installation

--- setup.sh
+++ setup.sh
@@ -2,10 +2,23 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
+# Install emacs
 sudo apt-get install -y emacs
 
-# 
+# Install virtualenvwrapper
 sudo apt-get install -y python-pip python-dev
 sudo pip install virtualenvwrapper
+
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git-core
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install uglify-js as a command line app (version 2.x)
+npm install uglify-js -g
